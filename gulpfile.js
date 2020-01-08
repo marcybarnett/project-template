6e8ee98ae1d6ea12897a2acaeac2d384aafda0cc
marcybarnett <barnettconcepts@gmail.com>
2020-01-07 23:12:36 -0700

    basic gulp file set up and functional

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,13 @@ const gulp = require('gulp'),
     rename = require('gulp-rename'),
     terser = require('gulp-terser'),
     images = require('gulp-imagemin'),
+    strip = require('gulp-strip-comments'),
     browserSync = require('browser-sync').create();
 
 // paths
 const styleSrc = './scss/**/*.scss',
     styleDest = './build/css/',
-    htmlSrc = './',
+    htmlSrc = './*.html',
     htmlDest = './build/',
     //vendorSrc = './js/vendors/',
     //vendorDest = './build/assets/js/',
@@ -57,6 +58,14 @@ function imageMin() {
 }
 exports.imageMin = imageMin;
 
+// strip comments from HTML
+function stripHtml() {
+    return gulp.src(htmlSrc)
+        .pipe(strip())
+        .pipe(gulp.dest(htmlDest));
+}
+exports.stripHtml = stripHtml;
+
 function watch() {
     // Serve files from the root of this project or local by flywheel
     browserSync.init({
@@ -64,19 +73,20 @@ function watch() {
         // open: true,
         server: {
             baseDir: "./build"
-        },
-        notify: false
+        }
     });
 
     // run our tasks
     gulp.watch(styleSrc, style);
     gulp.watch(scriptSrc, minifyScript);
     gulp.watch(imgSrc, imageMin);
+    gulp.watch(htmlSrc, stripHtml);
 
     // refresh browser
     gulp.watch(styleDest).on('change', browserSync.reload);
     gulp.watch(scriptDest).on('change', browserSync.reload);
     gulp.watch(imgDest).on('change', browserSync.reload);
+    gulp.watch(htmlDest).on('change', browserSync.reload);
 
 }
 exports.watch = watch;
